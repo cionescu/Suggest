11603d741a067695b4aa9ae2135d3418bb374c44
Catalin <newuser@example.com>
2014-06-20 11:00:46 +0100

    initial commit for suggest

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="suggest.js"></script>
+        <title>Suggestion plug-in demo</title>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                console.log(typeof $);
+            });
+        </script>
+    </head>
+    <body>
+        <?php echo 'catalin' ?>
+    </body>
+</html>
---
+++ suggest.js
@@ -0,0 +1,5 @@
+if((typeof $) !== 'function') {
+    throw "You need to include JQuery before this library";
+} else {
+    console.log("pre-requisites are okay!");
+}
